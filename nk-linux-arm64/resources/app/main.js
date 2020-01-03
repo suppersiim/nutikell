707ee7629555bb71e405e42828ab5f8aae57b42f
suppersiim <59343142+suppersiim@users.noreply.github.com>
2020-01-03 17:04:19 +0200

    commit arm64 sample

---
+++ nk-linux-arm64/resources/app/main.js
@@ -0,0 +1,60 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+const { app, BrowserWindow } = require('electron');
+const windowStateKeeper = require('electron-window-state');
+
+let mainWindow;
+
+if (!app.requestSingleInstanceLock()) {
+  app.quit();
+}
+
+function createWindow() {
+  const state = windowStateKeeper({
+    defaultWidth: 1240,
+    defaultHeight: 890,
+  });
+
+  mainWindow = new BrowserWindow({
+    x: state.x,
+    y: state.y,
+    width: state.width,
+    height: state.height,
+    //fullscreen: true,
+    show: false,
+    webPreferences: {
+      nodeIntegration: true,
+      //devTools: !app.isPackaged,
+    },
+  });
+
+  mainWindow.loadFile('app/app.html');
+  if (!app.isPackaged) mainWindow.webContents.openDevTools();
+  state.manage(mainWindow);
+
+  mainWindow.once('ready-to-show', () => mainWindow.show());
+
+  mainWindow.on('close', (e) => {
+    e.preventDefault();
+    mainWindow.hide();
+  });
+
+  mainWindow.on('closed', () => {
+    mainWindow = null;
+  });
+}
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform === 'darwin') return;
+  app.quit();
+});
+
+app.on('activate', () => {
+  if (mainWindow !== null) return;
+  createWindow();
+});
+
+app.on('second-instance', () => {
+  if (mainWindow) mainWindow.show();
+});
